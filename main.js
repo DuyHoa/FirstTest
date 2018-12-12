b5b107f62dd56bc01da8c7b4b2dc57719aae2c9e
DuyHoa <ndh.duyhoa.97@gmail.com>
2018-12-13 01:46:34 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,69 +1,36 @@
-var emoji_page_1 = {
-	icon_1: "";
-	icon_2: "";
-	icon_3: "";
-	icon_4: "";
-	icon_5: "";
-	icon_6: "";
-	icon_7: "";
-	icon_8: "";
-	icon_9: "";
-	icon_10: "";
-	icon_11: "";
-	icon_12: "";
-	icon_13: "";
-	icon_14: "";
-	icon_15: "";
-	icon_16: "";
-	icon_17: "";
-	icon_18: "";
-	icon_19: "";
-	icon_20: "";
-}
+var emojis = [0x1F600, 0x1F604, 0x1F34A, 0x1F344, 0x1F37F, 0x1F363, 0x1F370, 0x1F355,
+              0x1F354, 0x1F35F, 0x1F6C0, 0x1F48E, 0x1F5FA, 0x23F0, 0x1F579, 0x1F4DA,
+              0x1F431, 0x1F42A, 0x1F439, 0x1F424];
+
+
+
+//var myJson = JSON.stringify(t);
+//localStorage.setItem("ObjectJson", myJson);
+//var loadObj = localStorage.getItem("ObjectJson");
+//var obj = JSON.parse(loadObj);
+//var _count = obj.length;
 
 function set_style_(){
-	//css khung ngoai
-	var ske_emoji_out_= document.getElementById("Emoji_button");
-	ske_emoji_out_.style.width = "200px";
-	ske_emoji_out_.style.height = "180px";
-	//css khung trong
-	var ske_emoji_in_= document.getElementById("emoji_khung_trong");
-	ske_emoji_in_.style.border = "1px solid #ddd";
-	ske_emoji_in_.style.paddingLeft = "5px";
-	ske_emoji_in_.style.paddingRight = "5px";
-	ske_emoji_in_.style.paddingBottom = "5px";
-	ske_emoji_in_.style.paddingTop = "20px";
-	//css khung trong er
-	var ske_emoji_in_in_ = document.getElementById("emoji_khung_trong-er");
-	ske_emoji_in_in_.style.border = "1px solid #ddd";
-	ske_emoji_in_in_.style.padding = "3px";
-	ske_emoji_in_in_.style.overflow-y = "scroll";
-	
-	
-	var ske_border_emoji = document.getElementsByClassName("blockEmoji");
-	ske_border_emoji.style.width = "30px";
-	ske_border_emoji.style.height = "30px";
-	ske_border_emoji.style.padding = "3px";
-	//css emoji
-	var emoji_img = document.getElementById("emoji");
-	emoji_img.style.backgroundSize = "cover";
-	
+	var styleElement = document.createElement("style");
+	styleElement.appendChild(document.createTextNode("#emoji_khung_ngoai{position: absolute; width: 200px; height: 190px;} #_id_button_emoji{border-radius: 50%; margin-bottom: 5px; cursor: pointer; width: 25px; height: 25px; border: 1px solid #666; } #_id_button_emoji:hover {box-shadow: 0px 0px 3px 1px rgba(8, 8, 8, 0.17);} #emoji_khung_trong{ border: 1px solid #666; height: 132px; padding: 5px; padding-top: 20px;}#emoji_khung_trong-er{position: relative; border: 1px solid #666; height: 124px; padding: 3px; overflow-y: scroll;} #emoji{background-size: cover;} .blockEmoji{ width: 30px;height: 30px;padding: 3px;} .blockEmoji:hover {border: 1px solid #333;} #emoji_khung_trong-er::-webkit-scrollbar {width: 10px;}#emoji_khung_trong-er::-webkit-scrollbar-track {box-shadow: inset 0 0 5px grey; border-radius: 10px;}#emoji_khung_trong-er::-webkit-scrollbar-thumb{background: #666; border-radius: 10px;}#emoji_khung_trong-er::-webkit-scrollbar-thumb:hover{background: #333;}"));
+	document.getElementsByTagName("head")[0].appendChild(styleElement);
 }
 
 function create_ske_emoji(){
 	var ske_out = document.createElement("div");
 	ske_out.className = "Emoji_button";
 	ske_out.id = "emoji_khung_ngoai";
-	var a_out = document.getElementById("subject");
-	a_out[0].append(skeout);
+	var a_out = document.getElementsByTagName("body");
+	a_out[0].append(ske_out);
 }
 
 function create_ske_emoji_in(){
 	var ske_in = document.createElement("div");
 	ske_in.className = "Emoji_button";
 	ske_in.id = "emoji_khung_trong";
-	var a = document.getElementById("Emoji_button");
-	a[0].append(skeout);
+	ske_in.style.display = "none";
+	var a = document.getElementById("emoji_khung_ngoai");
+	a.append(ske_in);
 }
 
 function create_ske_emoji_in_in(){
@@ -71,45 +38,61 @@ function create_ske_emoji_in_in(){
 	ske_in_in.className = "Emoji_button";
 	ske_in_in.id = "emoji_khung_trong-er";
 	var a = document.getElementById("emoji_khung_trong");
-	a[0].append(skeout);
+	a.append(ske_in_in);
 }
 
 function add_button_insert_icon(){
-	var button_icon = document.createElement("input");
-	button_icon.className = "bt_emoji";
-	
-	var a = document.getElementById("emoji_khung_ngoai")[0];
-	a.appendChild(button_icon);
-	
-		
+	var button_icon = document.createElement("div");
+	button_icon.id = "_id_button_emoji";
+	var a = document.getElementById("emoji_khung_ngoai");
+	a.append(button_icon);	
 }
 
 function QuerryEmoji(){
-	for (var i = 0; i < emoji_page_1.length; i++){
+	var toAdd = document.createDocumentFragment();
+	for (var i = 0; i < emojis.length; i++){
 		//tao blocl emoji
 		var block_emoji = document.createElement("div");
 		block_emoji.className = "blockEmoji";
-		block_emoji.id = "emoji-"+i;
-		var b = document.getElementById("emoji_khung_trong-er");
-		b.appendChild(block_emoji);
-		var img = document.createElement("image");
+		block_emoji.innerHTML = emojis[i];
+		//block_emoji.background = t.array[key=i].src.icon;
+		toAdd.appendChild(block_emoji);
+		/*var img = document.createElement("image");
 		img.className = "emoji";
-		img.src = emoji_page_1.icon_+(i+1);
-		var a = document.getElementById("emoji-"+i");
-		a.appendChild(img);
+		img.src = t.array[key=i].src.icon;
+		block_emoji.appendChild("img");*/
 	}
+	document.getElementById("emoji_khung_trong-er").appendChild(toAdd);
 }
 
-
-function main(){
-	create_ske_emoji();
-	add_button_insert_icon();
-	create_ske_emoji_in();
-	create_ske_emoji_in_in();
-	set_style_();
-	QuerryEmoji();
-}
+function turn_on_of_table_emoji(){
+	document.getElementById("_id_button_emoji").onclick = function(){
+		if(document.getElementById("emoji_khung_trong").style.display == "none"){
+			document.getElementById("emoji_khung_trong").style.display = "block";
+		}
+		else
+			document.getElementById("emoji_khung_trong").style.display = "none";
+	}
 	
+}
+
+(function(){
+	window.onload = function(){
+		create_ske_emoji();
+		add_button_insert_icon();
+		create_ske_emoji_in();
+		create_ske_emoji_in_in();
+		QuerryEmoji();
+		set_style_();
+		turn_on_of_table_emoji();
+	}
+})();
 
 
-
+//SELECT * FROM cp.`*.json`
+//$.array[?(@.key=2)].src.icon
+/*{
+        dvar ctl = document.getElementById('Javascript_example');
+        var startPos = ctl.selectionStart;
+        var endPos = ctl.selectionEnd;
+      }*/
